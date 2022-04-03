2ae97be914426ee8e1609e8e60110f5c3cb83884
umar-bunu <umar.bunu73@gmail.com>
2022-04-03 16:01:11 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+char* encrypt(char* msg);
+char* decrypt(char* msg);
+int main() {
+
+    int shiftCount;
+    cout<<endl<<"Enter shift count: ";
+    cin >>shiftCount;
+
+
+    return 0;
+}
